a21964bc5ef1fccc3231cd49b999deb9947d801d
@slovacus <slovacus@gmail.com>
2019-12-28 13:30:56 -0500

    refactor: implement docker run (#1)

--- provision/make/tests.mk
+++ provision/make/tests.mk
@@ -11,20 +11,17 @@ test.help:
 	@echo '        test.syntax             Run all syntax in code'
 	@echo ''
 
-test: clean
+test:
 	@echo $(MESSAGE) Running tests on the current Python interpreter with coverage $(END)
-	@if [[ -z "${run}" ]]; then \
-		$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml run --rm \
-			app bash -c "$(PIPENV_RUN) python setup.py test" ; \
+	@if [ -z "${run}" ]; then \
+		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest"
 	fi
-	@if [[ -n "${run}" ]]; then \
-		$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/test.yml run --rm \
-			app bash -c "$(PIPENV_RUN) pytest tests/${run}" ; \
+	@if [ -n "${run}" ]; then \
+		$(docker-test-run) bash -c "$(PIPENV_RUN) pytest tests/${run}"
 	fi
 
-test.lint: clean
-	$(docker-compose) -f ${PATH_DOCKER_COMPOSE}/dev.yml run --rm \
-		app bash -c "$(PIPENV_RUN) pre-commit run --all-files --verbose"
+test.lint:
+	$(PIPENV_RUN) pre-commit run --all-files --verbose
 
-test.syntax: clean
+test.syntax:
 	@echo $(MESSAGE) Running tests $(END)
